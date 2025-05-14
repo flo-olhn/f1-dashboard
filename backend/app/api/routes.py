02c0fbb603a7290f401354fbb15b6885508b13a3
Florian Ouilhon <ouilhonflorian@outlook.fr>
2025-05-14 21:29:08 +0200

    start fetching data for driver standings

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
-from services.fastf1_service import get_driver_standings
+from services import fastf1_service
 
 router = APIRouter()
+ff1 = fastf1_service
 
 @router.get("/standings")
 def standings():
-    return get_driver_standings()
+    return {
+        #"events": ff1.get_events(2025),
+        "standings": ff1.get_driver_standings(2021),
+        #"constructor_std": ff1.get_constructor_standings() 
+    }
--- backend/app/services/fastf1_service.py
+++ backend/app/services/fastf1_service.py
@@ -1,6 +1,53 @@
-import fastf1
+import fastf1 as ff1
+import pandas as pd
+import numpy as np
+from datetime import date
+import datetime
 
-def get_driver_standings():
-    # Exemple de récupération des données de classement des pilotes
-    # Cette fonction peut être adaptée selon les données disponibles via FastF1
-    return {"message": "Classement des pilotes"}
+current_season = date.today().year
+
+
+def get_events(season: int = current_season):
+    if season == current_season:
+        return {
+            "passed_events": ff1.get_event_schedule(season, include_testing=False)[ff1.get_event_schedule(season, include_testing=False)['Session5DateUtc'] < datetime.datetime.now()].to_dict(orient='records'),
+            "upcoming_events": ff1.get_event_schedule(season, include_testing=False)[ff1.get_event_schedule(season, include_testing=False)['Session5DateUtc'] > pd.to_datetime(datetime.datetime.now())].to_dict(orient='records')
+        }
+    else:
+        return { "passed_events": ff1.get_event_schedule(season, include_testing=False).to_dict(orient='records') }
+
+def get_driver_standings(season: int = current_season):
+    global_ranking = pd.DataFrame()
+    races = []
+    for event in get_events(season)['passed_events']:
+        round_number = event['RoundNumber']
+        session = ff1.get_session(season, round_number, 'R')
+        session.load(telemetry=False, laps=False, weather=False, messages=False)
+        print(session.results['Position'].isna())
+        # replace NaN with None
+        session.results['Position'] = session.results['Position'].where(session.results['Position'].notna(), None)
+        races.append(session.results)
+
+    results = pd.concat(races)
+    summary = results.groupby(['DriverNumber', 'Abbreviation']).agg({'Points': 'sum', 'Position': list}).reset_index()
+    summary = summary.sort_values(by='Points', ascending=False)
+    for positions in summary['Position']:
+        for p in positions:
+            if p.isna():
+                p = None
+            else:
+                print(p)
+
+    #print(summary)
+    #for positions in summary['Position']:
+    #    for p in positions:
+    #        print(p == np.nan, p == float('nan'), p == None, p == 'nan', p == 'None')
+
+    return {
+        "season": season,
+        "driver_standings": summary.to_dict(orient='records'),
+    }
+
+def get_constructor_standings(season: int = date.today().year):
+    pass
+
