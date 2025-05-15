fa1fad12d4355c091f602abbf476695580ccc495
Florian Ouilhon <ouilhonflorian@outlook.fr>
2025-05-15 14:22:21 +0200

    cache

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -8,6 +8,6 @@ ff1 = fastf1_service
 def standings():
     return {
         #"events": ff1.get_events(2021),
-        "standings": ff1.get_standings(),
+        "standings": ff1.get_standings(2023),
         #"constructor_std": ff1.get_constructor_standings() 
     }
