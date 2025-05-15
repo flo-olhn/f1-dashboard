ee750d05a7f01e21c83dfbd0474a39fabaf3e22f
Florian Ouilhon <ouilhonflorian@outlook.fr>
2025-05-15 10:23:01 +0200

    fixed issue for nan values in Position list when fetching races results for driver standings

--- backend/app/services/fastf1_service.py
+++ backend/app/services/fastf1_service.py
@@ -17,31 +17,17 @@ def get_events(season: int = current_season):
         return { "passed_events": ff1.get_event_schedule(season, include_testing=False).to_dict(orient='records') }
 
 def get_driver_standings(season: int = current_season):
-    global_ranking = pd.DataFrame()
     races = []
     for event in get_events(season)['passed_events']:
         round_number = event['RoundNumber']
         session = ff1.get_session(season, round_number, 'R')
         session.load(telemetry=False, laps=False, weather=False, messages=False)
-        print(session.results['Position'].isna())
-        # replace NaN with None
-        session.results['Position'] = session.results['Position'].where(session.results['Position'].notna(), None)
+        session.results['Position'] = np.where(np.isnan(session.results['Position']), None, session.results['Position'])
         races.append(session.results)
 
     results = pd.concat(races)
     summary = results.groupby(['DriverNumber', 'Abbreviation']).agg({'Points': 'sum', 'Position': list}).reset_index()
     summary = summary.sort_values(by='Points', ascending=False)
-    for positions in summary['Position']:
-        for p in positions:
-            if p.isna():
-                p = None
-            else:
-                print(p)
-
-    #print(summary)
-    #for positions in summary['Position']:
-    #    for p in positions:
-    #        print(p == np.nan, p == float('nan'), p == None, p == 'nan', p == 'None')
 
     return {
         "season": season,
