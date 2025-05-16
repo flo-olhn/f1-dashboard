fc09824d51cf544b681132b977eab7f9761f1bf0
Florian Ouilhon <ouilhonflorian@outlook.fr>
2025-05-16 14:56:04 +0200

    update models

---
+++ Makefile
@@ -0,0 +1,2 @@
+update-db:
+	docker-compose exec api python update_db.py
--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -6,8 +6,7 @@ from database.database import Base
 class Season(Base):
     __tablename__ = "seasons"
 
-    id = Column(Integer, primary_key=True, index=True)
-    year = Column(Integer, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     created_at = Column(DateTime)
 
     events = relationship("Event", back_populates="season")
@@ -16,21 +15,20 @@ class Event(Base):
     __tablename__ = "events"
 
     id = Column(Integer, primary_key=True, index=True)
-    season_id = Column(Integer, ForeignKey("seasons.id"))
-    season_year = Column(Integer)
+    year = Column(Integer, ForeignKey("seasons.id"))
     round = Column(Integer)
     name = Column(String)
     location = Column(String)
     country = Column(String)
     session1 = Column(String)  # e.g., "Practice 1", "Qualifying", "
     session1_date = Column(DateTime)
-    session2 = Column(String)  # e.g., "Practice 1", "Qualifying", "
+    session2 = Column(String)  
     session2_date = Column(DateTime)
-    session3 = Column(String)  # e.g., "Practice 1", "Qualifying", "
+    session3 = Column(String)  
     session3_date = Column(DateTime)
-    session4 = Column(String)  # e.g., "Practice 1", "Qualifying", "
+    session4 = Column(String)  
     session4_date = Column(DateTime)
-    session5 = Column(String)  # e.g., "Practice 1", "Qualifying", "
+    session5 = Column(String) 
     session5_date = Column(DateTime)
     status = Column(String)  # "passed" or "upcoming"
     created_at = Column(DateTime)
