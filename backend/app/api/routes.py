075faeadf4d70053c93d86fe1ff6d9df372400f7
Florian Ouilhon <ouilhonflorian@outlook.fr>
2025-05-16 10:18:29 +0200

    updating routes

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,17 +1,35 @@
 from fastapi import APIRouter
 from services import fastf1_service
+from datetime import datetime
 
 router = APIRouter()
 ff1 = fastf1_service
 
+
 @router.get("/standings")
-def standings():
+async def standings():
     return {
-        "standings": ff1.get_standings(),
+        "standings": ff1.get_standings(datetime.now().year),
+    }
+
+@router.get("/standings/{season}")
+async def standings(season: int = None):
+    if season is None:
+        season = datetime.now().year
+    return {
+        "standings": ff1.get_standings(season),
     }
 
 @router.get("/events")
-def events():
+async def events():
     return {
-        "events": ff1.get_events()
+        "events": ff1.get_events(datetime.now().year)
+    }
+
+@router.get("/events/{season}")
+async def events(season: int = None):
+    if season is None:
+        season = datetime.now().year
+    return {
+        "events": ff1.get_events(season)
     }
